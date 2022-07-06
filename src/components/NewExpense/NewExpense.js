75d1be9a98f5e8cf87ce1cf8a5c6a55f7db12aea
Josh Nagel <nageljp27@gmail.com>
2022-07-06 17:34:03 -0600

    added a button to conditionally render adding a new expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,38 @@
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
+import { useState } from 'react'
 
 const NewExpense = ({ onAddExpense }) => {
+  const [isEditing, setIsEditing] = useState(false)
+
   const handleSaveExpenseData = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: parseInt(Math.random()).toString()
     }
     onAddExpense(expenseData)
+    setIsEditing(false)
   }
+
+  const handleEditingStart = () => {
+    setIsEditing(true)
+  }
+
+  const handleEditingStop = () => {
+    setIsEditing(false)
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={handleSaveExpenseData} />
+      {!isEditing && (
+        <button onClick={handleEditingStart}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={handleSaveExpenseData}
+          onCancel={handleEditingStop}
+        />
+      )}
     </div>
   )
 }
